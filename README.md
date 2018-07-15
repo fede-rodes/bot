2e780f18f53fe467f27c61c355709a7787979c8e
fede-rodes <rodesf@gmail.com>
2018-07-15 18:29:58 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,8 @@
+Node server for FB chatbot
+
+```
+>> npm install
+>> npm start
+```
+
+Got to http://localhost:3000
---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require('express');
+
+// Get env variables
+const { NODE_ENV, PORT = 3000, MONGO_URL } = process.env;
+
+// Log env variables
+console.log(
+  '\nNODE_ENV', NODE_ENV,
+  '\nPORT', PORT,
+  '\nMONGO_URL', MONGO_URL,
+)
+
+// Init node server
+const server = express();
+
+// Middlewares
+server.use(express.json());
+
+// Routes
+server.get('/', (req, res) => {
+  res.send('Hello');
+  res.end();
+});
+
+// Listen
+server.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT} ...`);
+});
